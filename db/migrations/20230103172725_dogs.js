2a168a10deb7757792627ea70c5640348a40ce17
Colin Ciervo <colinciervo@@gmail.com>
2023-01-04 10:07:25 -0500

    Change hyphens to underscores

--- db/migrations/20230103172725_dogs.js
+++ db/migrations/20230103172725_dogs.js
@@ -3,10 +3,10 @@ exports.up = knex =>
     tbl.increments();
     tbl.text("breed", 128).notNullable();
     tbl.text("size", 128).notNullable();
-    tbl.integer("family-rating", 128).notNullable();
+    tbl.integer("family_rating", 128).notNullable();
     tbl.integer("trainability", 128).notNullable();
-    tbl.integer("grooming-needs", 128).notNullable();
-    tbl.integer("energy-level", 128).notNullable();
+    tbl.integer("grooming_needs", 128).notNullable();
+    tbl.integer("energy_level", 128).notNullable();
     tbl.text("img", 256).notNullable();
   });
 
