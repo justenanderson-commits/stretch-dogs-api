9ae1136112c5a98466d7325c401b2dce2d039164
Colin Ciervo <colinciervo@@gmail.com>
2023-01-03 19:51:17 -0500

    Add columns to the dogs_dev table

--- db/migrations/20230103172725_dogs.js
+++ db/migrations/20230103172725_dogs.js
@@ -1,7 +1,13 @@
 exports.up = knex =>
   knex.schema.createTable("dogs", tbl => {
     tbl.increments();
-    tbl.text("task", 128).notNullable();
+    tbl.text("breed", 128).notNullable();
+    tbl.text("size", 128).notNullable();
+    tbl.integer("family-rating", 128).notNullable();
+    tbl.integer("trainability", 128).notNullable();
+    tbl.integer("grooming-needs", 128).notNullable();
+    tbl.integer("energy-level", 128).notNullable();
+    tbl.text("img", 256).notNullable();
   });
 
 exports.down = knex => knex.schema.dropTableIfExists("dogs"); 
