f3521358c748cf8dbd670e667ba8c70b161483ed
Justen Anderson <justenanderson@gmail.com>
2023-01-05 10:28:55 -0700

    Created db query for exact matches.

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const knex = require('knex')
 
 const knexfile = require('./knexfile')
 
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV || 'development'
 const configOptions = knexfile[env]
 
 module.exports = knex(configOptions)
