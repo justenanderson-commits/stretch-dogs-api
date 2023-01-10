a62a0734fa3c5468f9dbaf6fa29ba881642277c3
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-09 19:29:55 -0600

    Add field for description for table schema

--- db/migrations/20230103172725_dogs.js
+++ db/migrations/20230103172725_dogs.js
@@ -1,13 +1,14 @@
-exports.up = knex =>
-  knex.schema.createTable("dogs", tbl => {
-    tbl.increments();
-    tbl.text("breed", 128).notNullable();
-    tbl.integer("size", 128).notNullable();
-    tbl.integer("family_rating", 128).notNullable();
-    tbl.integer("trainability", 128).notNullable();
-    tbl.integer("grooming_needed", 128).notNullable();
-    tbl.integer("energy_level", 128).notNullable();
-    tbl.text("img", 256).notNullable();
-  });
+exports.up = (knex) =>
+  knex.schema.createTable('dogs', (tbl) => {
+    tbl.increments()
+    tbl.text('breed', 128).notNullable()
+    tbl.integer('size', 128).notNullable()
+    tbl.integer('family_rating', 128).notNullable()
+    tbl.integer('trainability', 128).notNullable()
+    tbl.integer('grooming_needed', 128).notNullable()
+    tbl.integer('energy_level', 128).notNullable()
+    tbl.text('img', 256).notNullable()
+    tbl.text('description').notNullable()
+  })
 
-exports.down = knex => knex.schema.dropTableIfExists("dogs"); 
+exports.down = (knex) => knex.schema.dropTableIfExists('dogs')
